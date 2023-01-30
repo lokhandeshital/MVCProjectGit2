d579a4aab27e0e90e18f12ddeedab12656b649a1
Shital lokhande <lokhandeshital.com>
2023-01-30 19:30:24 +0530

    changes by Shital

---
+++ src/main/java/com/BikkadIT/GitHub2Application.java
@@ -0,0 +1,13 @@
+package com.BikkadIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHub2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHub2Application.class, args);
+	}
+
+}
